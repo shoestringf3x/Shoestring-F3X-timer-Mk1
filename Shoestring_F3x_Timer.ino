a8f829d0b82771a5e41695500908330ca6df8d11
shoestringf3x <hans.birgander@gmail.com>
2017-07-22 22:23:36 +0200

    Delete Shoestring_F3x_Timer.ino

--- Shoestring_F3x_Timer.ino
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------
-// Shoestring F3x Timer program for Arduino
-// Created 2017 by Hans Birgander (hans@birgander.se)
-// Copyright, all rights reserved.
-// 
-// This program is used to time flight for F3B, F3F (planned), F3J and F5J (planned)
-// RC model competition flights
-// 
-// Version 1.0 - Build 1
